87c1185afdd45314bac123356c6e9a1fb88db32a
mngdnmw <mtdunmow@gmail.com>
2017-08-23 09:24:52 +0200

    End of Day 4 commit

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day4
 {
@@ -7,49 +8,76 @@ namespace Day4
         public static void Main(string[] args)
         {
             Introduction();
-            string[] menuItems=new string[4];
-            AddItems(ref menuItems);
-            ShowMenu(menuItems);
+
+            List<string> menuItems = new List<string> { "Create video", "Read video", "Update video", "Delete video", "Exit" };
+
+            var selection = 0; 
+
+            while (selection !=5){
+                selection=ShowMenu(menuItems);
+				PrintSelection(selection);
+            }
+
+            Console.WriteLine("Hej hej!");
 
         }
 
-        private static int ShowMenu(string[] menuItems)
+        private static int ShowMenu(List<string> menuItems)
         {
-            Console.Clear();
+            //Console.Clear();
 
             Console.WriteLine("Select what you want to do:\n");
-            for (int i = 0; i < menuItems.Length; i++)
+            for (int i = 0; i < menuItems.Count; i++)
             {
-                Console.WriteLine((i+1)+":"+menuItems[i]);
+                //Less memory heavy than the + signs
+                Console.WriteLine($"{ (i + 1)}:{ menuItems[i]}");
             }
 
-			Console.WriteLine( "");
+            Console.WriteLine("");
 
             int selection;
-            while (!int.TryParse(Console.ReadLine(), out selection)|| selection<1 || selection>5)
+
+            while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > 5)
             {
-                Console.WriteLine("You need to select one of the menu items by typing in the menu item nummber");
+                Console.WriteLine("You need to select one of the menu items by typing in the menu item number (between 1-5)\n");
             }
 
-            Console.WriteLine("Selection: " + selection);
             return selection;
 
         }
 
-        private static void AddItems(ref string[] menuItems)
+        private static void PrintSelection(int selection)
         {
-           menuItems. = {
-                "Create video",
-                "Read video",
-                "Update video",
-                "Delete video",
-                "Exit"
-            };     
+            switch (selection)
+            {
+                case 1:
+                    Console.WriteLine("Create video\n");
+                    break;
+                case 2:
+                    Console.WriteLine("Read video\n");
+                    break;
+                case 3:
+                    Console.WriteLine("Update video\n");
+                    break;
+                case 4:
+                    Console.WriteLine("Delete video\n");
+                    break;
+                default:
+                    break;
+            };
+
+        }
+
+        //ref works similiarly to out, but it requires initialisation before use 
+        private static void AddItems(ref List<string> menuItems, string item)
+        {
+            menuItems.Add(item);
+
         }
 
         private static void Introduction()
         {
-            Console.WriteLine("Welcome to the world's best video application\xB2a \n\n\xB2aJust better than Jeppe's");
+            Console.WriteLine("Welcome to the world's best video application\xB2a \n\xB2aJust better than Jeppe's\n");
         }
 
 
---
+++ Day4/Video.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Day4
+{
+    public class Video
+    {
+        //public string Name { get; set; }
+
+        //public int D
+
+
+    }
+}
